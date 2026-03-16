3e1822b43724e6bb7666a169abd8d4d784a4b5e1
K Yamaguchi | Web Dev in Training <k.yamaguchi.dev@gmail.com>
2026-03-16 10:51:46 +0900

    Update SQL-practice.sql
    リレーションを指定する書き方

--- SQL-practice.sql
+++ SQL-practice.sql
@@ -29,5 +29,17 @@ INSERT INTO 部署テーブル
 VALUES('B002','総務部');
 --select文
 SELECT * FROM `社員テーブル` WHERE 部署コード = 'B001';
+--RDB
+SELECT * FROM 
+(テーブル1 INNER JOIN テーブル2
+ON テーブル1.項目名 = テーブル2.項目名)
+-- 社員テーブル.部署コード
+-- 部署テーブル.部署コード
+WHERE テーブル名.項目名 = 条件
+-- ↓リアルな書き方
+SELECT * FROM 
+(社員テーブル INNER JOIN 部署テーブル
+ON 社員テーブル.部署コード = 部署テーブル.部署コード)
+WHERE 部署テーブル.部署名 = 'システム開発部';
 
 
